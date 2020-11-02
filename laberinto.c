e14f381fc73ee3810faa2d67e737fd24e99c3582
Irving Mondragón <mirvingr@gmail.com>
2020-11-01 22:10:22 -0600

    Replace windows functions and printable characters

--- laberinto.c
+++ laberinto.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#include <windows.h>
+#include <unistd.h>
+
+#define MILLISECONDS 1000 // microseconds
 
 void *gestiona_mem(int);
 void lim_filas( int **, int);
 void lim_columnas( int **, int);
 int lee_inicio( int **, int);
 int lee_fin( int **, int);
+void msleep(int);
 void imprime_matriz( int **, int, int, int);
 void genera_muros ( int **, int, int, int);
 void genera_camino ( int **, int);
@@ -28,41 +31,40 @@ int main()
     
     bienvenida();
     do{
-       Sleep(2000);
-       system("cls");
-       encabezado();
-       do{
-          printf("Selecciona el modo de juego:\n\n");
-          printf("1. Laberinto 12 x 12\n2. Laberinto n x n\n\n");
-          printf("Opcion: ");
-          scanf ("%d", &op);
-          if (op != 1 && op != 2)
-             printf("Error! Ingrese otra opcion.\n\n"); 
-          Sleep(1000);
+        msleep(2000);
+        system("clear");
+        encabezado();
+        do{
+            printf("Selecciona el modo de juego:\n\n");
+            printf("1. Laberinto 12 x 12\n2. Laberinto n x n\n\n");
+            printf("Opcion: ");
+            scanf ("%d", &op);
+            if (op != 1 && op != 2)
+                printf("Error! Ingrese otra opcion.\n\n"); 
+            msleep(2000);
        }while(op != 1 && op != 2);
        
-       if (op == 1)
-       {
-          dim = 12;
-          inicio = 1;
-          fin = 12;
-          dim += 2;
+       if (op == 1) {
+            dim = 12;
+            inicio = 1;
+            fin = 12;
+            dim += 2;
        }
        else{   
-           printf ("\nIngresa la dimension del laberinto: \n");
-           printf("Dimension: ");
-           scanf ("%d", &dim);
-           Sleep(200);
-           system("cls");
-           dim += 2;
-           inicio = lee_inicio(Laberinto, dim);
-           fin = lee_fin(Laberinto, dim);
+            printf ("\nIngresa la dimension del laberinto: \n");
+            printf("Dimension: ");
+            scanf ("%d", &dim);
+            msleep(200);
+            system("clear");
+            dim += 2;
+            inicio = lee_inicio(Laberinto, dim);
+            fin = lee_fin(Laberinto, dim);
        }
 
        //Reserva memoria
        Laberinto = (int **) gestiona_mem(sizeof(int *) * dim);
        for ( i = 0; i < dim; i++)
-           Laberinto[i] = (int *) gestiona_mem(sizeof(int)* dim);
+            Laberinto[i] = (int *) gestiona_mem(sizeof(int)* dim);
     
        genera_laberinto(Laberinto,dim,inicio,fin);
        genera_muros(Laberinto, dim, inicio, fin);
@@ -73,23 +75,23 @@ int main()
        //Libera memoria
        for ( i = 0; i < dim; i++)
             free(Laberinto[i]);
-    
        free(Laberinto);
        
-       do{      
-                printf("\nDesea jugar de nuevo? [1.Si | 2.No]: \n");
-                printf("Opcion: ");
-                scanf("%d",&bandera);
-                if (bandera != 1 && bandera != 2)
-                   printf("Error! Ingrese otra opcion.\n\n");
-       }while(bandera != 1 && bandera != 2);
+       do {      
+            printf("\nDesea jugar de nuevo? [1.Si | 2.No]: \n");
+            printf("Opcion: ");
+            scanf("%d",&bandera);
+            if (bandera != 1 && bandera != 2)
+                printf("Error! Ingrese otra opcion.\n\n");
+       } while(bandera != 1 && bandera != 2);
     }while(bandera !=2);
-    system("cls");
     despedida();
   
-    getchar();
-    getchar();
     return 0;
+}
+
+void msleep(int time) {
+    usleep(time * MILLISECONDS);
 }
 
 void *gestiona_mem(int elem)
@@ -145,11 +147,11 @@ int lee_inicio( int **Matriz, int tam)
         scanf("%d",&pI);
         
         if (pI <= 0 || pI > tam-2)
-              printf("Error! Ingrese otra posicion de inicio.\n");
+            printf("Error! Ingrese otra posicion de inicio.\n");
         
-        Sleep(1000);
+        msleep(1000);
         fflush(stdin);
-        system("cls");
+        system("clear");
      }while(pI <= 0 || pI > tam-2);
     
     return pI;
@@ -172,9 +174,9 @@ int lee_fin( int **Matriz, int tam)
         scanf("%d",&pF);
         
         if (pF<= 0 || pF > tam-2)
-              printf("Error! Ingrese otra posicion de salida.\n");
+            printf("Error! Ingrese otra posicion de salida.\n");
         
-        Sleep(1000);
+        msleep(1000);
         fflush(stdin);
         system("cls");
      }while(pF <= 0 || pF > tam-2);
@@ -192,14 +194,14 @@ void imprime_matriz( int **Matriz, int tam, int x, int y)
          printf("  ");
          for (j = 0; j < tam; j++)
          {
-             if ( Matriz[i][j] == 1 || Matriz[i][j] == 2)
-                    printf("%c%c", 178, 178); 
-             else
+            if ( Matriz[i][j] == 1 || Matriz[i][j] == 2)
+                printf("%c%c", '#','#'); // muro
+            else
              	if( i == x && j == y)
-                         printf("%c ", 1);
-                 else
+                    printf("%c ", '@'); // puntero
+                else
                  	if (Matriz[i][j] == 6 || Matriz[i][j] == 3)
-    	            	printf ("%c%c", 176, 176 );
+    	            	printf ("%c%c", '~','~'); // trayectoria
                     else
                         printf ("  ");
 		}
@@ -216,10 +218,10 @@ void genera_muros ( int **Matriz, int tam, int start, int finish)
      for(j = 1; j < tam-2; j++)
            for (i = 1; i < tam-1; i++)
            {
-               if (Matriz[i][j] != 5) 
-                  Matriz[i][j] = rand () % 2;
+                if (Matriz[i][j] != 5) 
+                    Matriz[i][j] = rand () % 2;
                 else
-                  Matriz[i][j] = 0;
+                    Matriz[i][j] = 0;
            }
            
 }
@@ -276,87 +278,89 @@ void resuelve_lab( int **Matriz, int tam, int i, int j, int finish, int orientac
      else
      {
          switch(orientacion){
-                             case 1:
-                                  if(Matriz[i][j+1] == 1 || Matriz[i][j+1] == 2)
-                                     if(Matriz[i-1][j] == 0 || Matriz[i-1][j] == 6)
-                                     {
-                                                       Matriz[i-1][j] = 6;
-                                                       i--;
-                                     }
-                                     else
-                                     {
-                                         orientacion = 2;
-                                     }
-                                  else
-                                  {
-                                      Matriz[i][j+1] = 6;
-                                      j++;
-                                      orientacion = 4;
-                                  }
-                                  break;
-                             case 2:
-                                  if (Matriz[i-1][j] == 1 || Matriz[i-1][j] == 2)
-                                     if(Matriz[i][j-1] == 0 || Matriz[i][j-1] == 6)
-                                     {
-                                                       Matriz[i][j-1] = 6;
-                                                       j--;
-                                     }
-                                     else
-                                     {
-                                         orientacion = 3;
-                                     }
-                                  else
-                                  {
-                                      Matriz[i-1][j] = 6;
-                                      i--;
-                                      orientacion = 1;
-                                  }
-                                  break; 
-                             case 3:
-                                  if (Matriz[i][j-1] == 1 || Matriz[i][j-1] == 2)
-                                     if(Matriz[i+1][j] == 0 || Matriz[i+1][j] == 6)
-                                     {
-                                                       Matriz[i+1][j] = 6;
-                                                       i++;
-                                     }
-                                     else
-                                     {
-                                         orientacion = 4;
-                                     }
-                                  else
-                                  {
-                                      Matriz[i][j-1] = 6;
-                                      j--;
-                                      orientacion = 2;
-                                  }
-                                  break;    
-                                  
-                             case 4:
-                                  if (Matriz[i+1][j] == 1 || Matriz[i+1][j] == 2)
-                                     if(Matriz[i][j+1] == 0 || Matriz[i][j+1] == 6)
-                                     {
-                                                 Matriz[i][j+1] = 6;
-                                                 j++;
-                                     }
-                                     else
-                                     {
-                                         orientacion = 1;
-                                     }
-                                  else
-                                  {
-                                      Matriz[i+1][j] = 6;
-                                      i++;
-                                      orientacion = 3;
-                                  }
-                                  break;
+            case 1:
+                if(Matriz[i][j+1] == 1 || Matriz[i][j+1] == 2)
+                    if(Matriz[i-1][j] == 0 || Matriz[i-1][j] == 6)
+                    {
+                        Matriz[i-1][j] = 6;
+                        i--;
+                    }
+                    else
+                    {
+                        orientacion = 2;
+                    }
+                else
+                {
+                    Matriz[i][j+1] = 6;
+                    j++;
+                    orientacion = 4;
+                }
+                break;
+            case 2:
+                if (Matriz[i-1][j] == 1 || Matriz[i-1][j] == 2)
+                    if(Matriz[i][j-1] == 0 || Matriz[i][j-1] == 6)
+                    {
+                        Matriz[i][j-1] = 6;
+                        j--;
+                    }
+                    else
+                    {
+                        orientacion = 3;
+                    }
+                else
+                {
+                    Matriz[i-1][j] = 6;
+                    i--;
+                    orientacion = 1;
+                }
+                break; 
+            case 3:
+                if (Matriz[i][j-1] == 1 || Matriz[i][j-1] == 2)
+                    if(Matriz[i+1][j] == 0 || Matriz[i+1][j] == 6)
+                    {
+                        Matriz[i+1][j] = 6;
+                        i++;
+                    }
+                    else
+                    {
+                        orientacion = 4;
+                    }
+                else
+                {
+                    Matriz[i][j-1] = 6;
+                    j--;
+                    orientacion = 2;
+                }
+                break;    
+                
+            case 4:
+                if (Matriz[i+1][j] == 1 || Matriz[i+1][j] == 2)
+                    if(Matriz[i][j+1] == 0 || Matriz[i][j+1] == 6)
+                    {
+                        Matriz[i][j+1] = 6;
+                        j++;
+                    }
+                    else
+                    {
+                        orientacion = 1;
+                    }
+                else
+                {
+                    Matriz[i+1][j] = 6;
+                    i++;
+                    orientacion = 3;
+                }
+                break;
          }
-         Sleep(200);
-         system("cls");
+
+         msleep(200);
+         system("clear");
          imprime_matriz(Matriz, tam, i, j);
          printf("\nResolviendo laberinto... ");
          resuelve_lab(Matriz, tam, i, j, finish, orientacion);
      }
 }
+
 void genera_laberinto(int**Matriz, int tam, int start, int finish)
 {
      int i,j;
@@ -429,5 +433,3 @@ void despedida(void)
     printf("          * *                                           \n");
     printf("            *                                           \n");
 }
-          
-
